21a167c33372685ae7bf8826fc56c8092aa05187
Dixita Patel <dixitapatel24@gmail.com>
2019-04-09 18:55:23 -0400

    first commit

---
+++ bootjpa/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into alien values(101 ,'dixita', 'java');
+insert into alien values(102 ,'dhaval', 'java');
+insert into alien values(103 ,'dhavu', 'java');
+insert into alien values(104 ,'mukti', '.net');
+insert into alien values(105 ,'dhruti', 'Android');
+
